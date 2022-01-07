80679d27b90e5b73bcffff49c4843e7e9fac10d0
Saji Weerasingham <TheMagnificent11@users.noreply.github.com>
2022-01-08 01:23:35 +1000

    Salutation Data Seeder (#2)

---
+++ src/Acme.ClubManagement.Domain/Data/Seeders/EnumDataSeeder.cs
@@ -0,0 +1,30 @@
+﻿using Acme.ClubManagement.Domain;
+
+namespace Acme.ClubManagement.Data.Seeders;
+
+public static class EnumDataSeeder
+{
+    public static TRefData[] GetEnumReferenceData<TRefData, TEnum>()
+        where TRefData : BaseEnumEntity<TEnum>, new()
+        where TEnum : struct, IConvertible, IComparable
+    {
+        var result = new List<TRefData>();
+
+        foreach (TEnum item in Enum.GetValues(typeof(TEnum)))
+        {
+            var enumVal = (int)(object)item;
+
+            if (enumVal == 0)
+            {
+                continue;
+            }
+
+            var data = new TRefData();
+            data.SetId(item);
+
+            result.Add(data);
+        }
+
+        return result.ToArray();
+    }
+}
