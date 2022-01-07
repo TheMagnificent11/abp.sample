1135fb99d0487db175021a18f5efeade266a987e
Saji Weerasingham <saji.weerasingham@outlook.com>
2022-01-08 01:45:58 +1000

    person app service

---
+++ src/Acme.ClubManagement.Application.Contracts/People/IPersonAppService.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.ClubManagement.People;
+
+public interface IPersonAppService :
+    ICrudAppService<PersonDto, Guid, PagedAndSortedResultRequestDto, SavePersonRequest>
+{
+}
